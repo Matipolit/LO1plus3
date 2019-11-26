0a87fb7894dd66ac633a7408919288cf93d155e6
Matipolit <matipolit@gmail.com>
2019-11-26 21:20:35 +0100

    Started implementing SafeApiRequest Bug fixes

--- app/src/main/java/pl/matmar/matipolit/lo1plus/data/network/SafeApiRequest.kt
+++ app/src/main/java/pl/matmar/matipolit/lo1plus/data/network/SafeApiRequest.kt
@@ -1,2 +1,15 @@
 package pl.matmar.matipolit.lo1plus.data.network
 
+import retrofit2.Response
+
+abstract class SafeApiRequest {
+    suspend fun <T : Any> apiRequest(call: suspend() -> Response<T>) :T{
+        val response = call.invoke()
+
+        if(response.isSuccessful){
+            return response.body()!!
+        }else{
+            val error = response.errorBody()?.string()
+        }
+    }
+}
--- app/src/main/java/pl/matmar/matipolit/lo1plus/data/network/responses.kt
+++ app/src/main/java/pl/matmar/matipolit/lo1plus/data/network/responses.kt
@@ -1,7 +1,7 @@
 package pl.matmar.matipolit.lo1plus.data.network
 
 data class AuthResponse(
-    val correct : Boolean?,
+    val correct : String,
     //val home: Home?,
     val admin: Boolean?,
     val obiadyAdmin: Boolean?,
