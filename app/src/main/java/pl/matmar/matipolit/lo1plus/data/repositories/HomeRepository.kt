c2e22c91b7374196ea63abb1159033cf25c9075f
Matipolit <matipolit@gmail.com>
2019-12-05 20:12:42 +0100

    started implementing homeRepository

--- app/src/main/java/pl/matmar/matipolit/lo1plus/data/repositories/HomeRepository.kt
+++ app/src/main/java/pl/matmar/matipolit/lo1plus/data/repositories/HomeRepository.kt
@@ -1,5 +1,14 @@
 package pl.matmar.matipolit.lo1plus.data.repositories
 
-class HomeRepository {
+import pl.matmar.matipolit.lo1plus.data.database.LO1Database
+import pl.matmar.matipolit.lo1plus.data.network.MyApi
+import pl.matmar.matipolit.lo1plus.data.network.SafeApiRequest
+
+class HomeRepository(private val api: MyApi,
+                     private val database: LO1Database
+) : SafeApiRequest(){
+    suspend fun homeResponse(){
+        
+    }
 
 }
